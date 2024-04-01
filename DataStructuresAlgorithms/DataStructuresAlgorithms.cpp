a441eb3af6d02192b7c27709182039034d1c62e6
Chris Worcester <chrisworc@gmail.com>
2024-03-31 22:34:22 -0400

    Use array utils in driver function

--- DataStructuresAlgorithms/DataStructuresAlgorithms.cpp
+++ DataStructuresAlgorithms/DataStructuresAlgorithms.cpp
@@ -1,31 +1,39 @@
 #include <iostream>
-#include "Search/BinarySearch.h"
+#include "ArrayUtils.h"
 
 int main()
 {
-	// Test QuickSort
-	//#include "Sort/QuickSort.h"
+	// Test QuickSort    #include "Sort/QuickSort.h"
 	//int arr[] = { 10, 7, 8, 9, 1, 5 };
 	//int n = sizeof(arr) / sizeof(arr[0]);
 	//std::cout << "Original array: ";
 	//QuickSort qs;
-	//qs.printArray(arr, n);
+	//ArrayUtils<int, 6>::print(arr);
 	//qs.quickSort(arr, 0, n - 1);
 	//std::cout << "Sorted array: ";
-	//qs.printArray(arr, n);
+	//ArrayUtils<int, 6>::print(arr);
 	//return 0;
 
-	// Test Binary Search
-	int arr[] = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
-	int size = sizeof(arr) / sizeof(arr[0]); // Calculate size of array
-	int target = 12;
+	// Test Binary Search    #include "Search/BinarySearch.h"
+	//int arr[] = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
+	//int size = sizeof(arr) / sizeof(arr[0]); // Calculate size of array
+	//int target = 12;
+	//BinarySearch bs;
+	//int result = bs.binarySearch(arr, size, target);
+	//if (result != -1)
+	//	std::cout << "Element found at index: " << result << std::endl;
+	//else
+	//	std::cout << "Element not found" << std::endl;
+	//return 0;
 
-	BinarySearch bs;
-	int result = bs.binarySearch(arr, size, target);
-	if (result != -1)
-		std::cout << "Element found at index: " << result << std::endl;
-	else
-		std::cout << "Element not found" << std::endl;
-
-	return 0;
+	// Test Merge Sort    #include "Sort/MergeSort.h"
+	//int arr[] = { 12, 11, 13, 5, 6, 7 };
+	//int arr_size = sizeof(arr) / sizeof(arr[0]);
+	//std::cout << "Given array is \n";
+	//ArrayUtils<int, 6>::print(arr);
+	//MergeSort ms;
+	//ms.mergeSort(arr, 0, arr_size - 1);
+	//std::cout << "Sorted array is \n";
+	//ArrayUtils<int, 6>::print(arr);
+	//return 0;
 }
--- DataStructuresAlgorithms/Sort/QuickSort.h
+++ DataStructuresAlgorithms/Sort/QuickSort.h
@@ -20,13 +20,6 @@ public:
 	// The main function that implements QuickSort
 	void quickSort(int arr[], int low, int high);
 
-	void printArray(int arr[], int size)
-	{
-		for (int i = 0; i < size; i++)
-			std::cout << arr[i] << " ";
-		std::cout << std::endl;
-	}
-
 private:
 	// Function to swap two elements in the array
 	void swap(int* a, int* b);
