37e896137b0668351831b81b8f5853ef2e97fa24
Chris Worcester <chrisworc@gmail.com>
2024-03-31 20:42:36 -0400

    Cleanup and comments

--- DataStructuresAlgorithms/Search/BinarySearch.h
+++ DataStructuresAlgorithms/Search/BinarySearch.h
@@ -1,4 +1,11 @@
 #pragma once
+
+// Quick Sort is known for its efficient sorting algorithm that follows the Divide and Conquer strategy.
+// It works by selecting a 'pivot' element from the array and partitioning the other elements into two
+// sub-arrays according to whether they are less than or greater than the pivot. The sub-arrays are then
+// recursively sorted. Quick Sort is generally fast in practice and is widely used for sorting large datasets.
+// However, it may not be suitable for small datasets or datasets with many duplicate elements,
+// as it can degrade to O(n^2) time complexity in the worst case.
 class BinarySearch
 {
 public:
