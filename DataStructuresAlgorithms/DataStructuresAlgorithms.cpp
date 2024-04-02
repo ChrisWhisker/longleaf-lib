fa27c6731d5026aa68ddebc83406327440f24d81
Chris Worcester <chrisworc@gmail.com>
2024-04-02 00:32:07 -0400

    Reorganize driver code

--- DataStructuresAlgorithms/DataStructuresAlgorithms.cpp
+++ DataStructuresAlgorithms/DataStructuresAlgorithms.cpp
@@ -3,39 +3,7 @@
 
 int main()
 {
-	// Test QuickSort    #include "Sort/QuickSort.h"
-	// int arr[] = { 10, 7, 8, 9, 1, 5 };
-	// int n = sizeof(arr) / sizeof(arr[0]);
-	// std::cout << "Original array: ";
-	// QuickSort qs;
-	// ArrayUtils<int, 6>::print(arr);
-	// qs.quickSort(arr, 0, n - 1);
-	// std::cout << "Sorted array: ";
-	// ArrayUtils<int, 6>::print(arr);
-	// return 0;
-
-	// Test Binary Search    #include "Search/BinarySearch.h"
-	// int arr[] = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
-	// int size = sizeof(arr) / sizeof(arr[0]); // Calculate size of array
-	// int target = 12;
-	// BinarySearch bs;
-	// int result = bs.binarySearch(arr, size, target);
-	// if (result != -1)
-	//	std::cout << "Element found at index: " << result << std::endl;
-	// else
-	//	std::cout << "Element not found" << std::endl;
-	// return 0;
-
-	// Test Merge Sort    #include "Sort/MergeSort.h"
-	// int arr[] = { 12, 11, 13, 5, 6, 7 };
-	// int arr_size = sizeof(arr) / sizeof(arr[0]);
-	// std::cout << "Given array is \n";
-	// ArrayUtils<int, 6>::print(arr);
-	// MergeSort ms;
-	// ms.mergeSort(arr, 0, arr_size - 1);
-	// std::cout << "Sorted array is \n";
-	// ArrayUtils<int, 6>::print(arr);
-	// return 0;
+	// ********** DATA STRUCTURES **********
 
 	// Test LinkedList    #include "DataStructures/LinkedList.h"
 	//// Create a LinkedList object for integers
@@ -65,5 +33,43 @@ int main()
 	// list.display(); // Expected output: 3 10 4 5
 	//// Test length function
 	// std::cout << "Length of the list: " << list.length() << std::endl; // Expected output: 4
+	// return 0;
+
+	// ********** SEARCH **********
+
+	// Test Binary Search    #include "Search/BinarySearch.h"
+	// int arr[] = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
+	// int size = sizeof(arr) / sizeof(arr[0]); // Calculate size of array
+	// int target = 12;
+	// BinarySearch bs;
+	// int result = bs.binarySearch(arr, size, target);
+	// if (result != -1)
+	//	std::cout << "Element found at index: " << result << std::endl;
+	// else
+	//	std::cout << "Element not found" << std::endl;
+	// return 0;
+
+	// ********** SORT **********
+
+	// Test Merge Sort    #include "Sort/MergeSort.h"
+	// int arr[] = { 12, 11, 13, 5, 6, 7 };
+	// int arr_size = sizeof(arr) / sizeof(arr[0]);
+	// std::cout << "Given array is \n";
+	// ArrayUtils<int, 6>::print(arr);
+	// MergeSort ms;
+	// ms.mergeSort(arr, 0, arr_size - 1);
+	// std::cout << "Sorted array is \n";
+	// ArrayUtils<int, 6>::print(arr);
+	// return 0;
+
+	// Test QuickSort    #include "Sort/QuickSort.h"
+	// int arr[] = { 10, 7, 8, 9, 1, 5 };
+	// int n = sizeof(arr) / sizeof(arr[0]);
+	// std::cout << "Original array: ";
+	// QuickSort qs;
+	// ArrayUtils<int, 6>::print(arr);
+	// qs.quickSort(arr, 0, n - 1);
+	// std::cout << "Sorted array: ";
+	// ArrayUtils<int, 6>::print(arr);
 	// return 0;
 }
