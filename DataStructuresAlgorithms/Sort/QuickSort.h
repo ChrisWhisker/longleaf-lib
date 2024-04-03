d7fddf94316c6c31970d520b63e24ee4cf5949c4
Chris Worcester <chrisworc@gmail.com>
2024-04-02 23:29:15 -0400

    Make functions inline

--- DataStructuresAlgorithms/Sort/QuickSort.h
+++ DataStructuresAlgorithms/Sort/QuickSort.h
@@ -16,12 +16,44 @@ class QuickSort
 {
 public:
 	// The main function that implements QuickSort
-	void quickSort(int arr[], int low, int high);
+	void quickSort(int arr[], int low, int high)
+	{
+		if (low < high) // If there are more than one elements in the subarray
+		{
+			// arr[p] is now at right place
+			const int partitionIndex = partition(arr, low, high);
+
+			// Recursively sort elements before and after the partition
+			quickSort(arr, low, partitionIndex - 1);  // Sort the left subarray
+			quickSort(arr, partitionIndex + 1, high); // Sort the right subarray
+		}
+	}
 
 private:
 	// Function to swap two elements in the array
-	void swap(int* a, int* b) noexcept;
+	void swap(int* a, int* b) noexcept
+	{
+		const int temp = *a;
+		*a = *b;
+		*b = temp;
+	}
 
 	// Partition function to rearrange the array so that elements smaller than pivot are on the left and larger than pivot are on the right
-	int partition(int arr[], int low, int high);
+	int partition(int arr[], int low, int high)
+	{
+		const int pivot = arr[high]; // Choose the last element of the array as the pivot
+		int i = low - 1;	   // Initialize the index of the smaller element to one less than the low index
+
+		for (int j = low; j <= high - 1; j++)
+		{
+			// If current element is smaller than the pivot
+			if (arr[j] < pivot)
+			{
+				i++; // Increment index of smaller element
+				swap(&arr[i], &arr[j]);
+			}
+		}
+		swap(&arr[i + 1], &arr[high]); // Swap the pivot element with the element at index 'i + 1'
+		return (i + 1);				   // Return the partitioning index
+	}
 };
